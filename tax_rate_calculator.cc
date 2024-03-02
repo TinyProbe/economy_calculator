d388477b6ac1bb2d3619f91deec519a0c85716d9
TinyProbe <137adv167@gmail.com>
2024-03-02 09:18:44 +0900

    Update tax_rate_calculator.cc

--- tax_rate_calculator.cc
+++ tax_rate_calculator.cc
@@ -9,61 +9,37 @@ using namespace std;
 int argument_check(int ac,
                    char **av,
                    long long &n,
-                   long long &mn,
-                   long long &mx,
                    long long &benchmark,
                    double &tax_rate) {
-  if (ac != 6) {
+  if (ac != 4) {
     cerr << "invaild argument:" << '\n';
     cerr << "  usage:" << '\n';
-    cerr << "    ./[a.exe] <n> <min> <max> <tax_rate> <benchmark>" << '\n';
+    cerr << "    ./[a.exe] <n> <tax_rate> <benchmark>" << '\n';
     cerr << "    <n>: Number of social members." << '\n';
-    cerr << "    <min>: Minimum income." << '\n';
-    cerr << "    <max>: Maximum income." << '\n';
     cerr << "    <benchmark>: Benchmark max income." << '\n';
     cerr << "    <tax_rate>: Max tax rate in next system." << '\n';
     return -1;
   }
   n = atoll(av[1]);
-  mn = atoll(av[2]);
-  mx = atoll(av[3]);
-  benchmark = atoll(av[4]);
-  tax_rate = atof(av[5]);
-  if (n < (long long)1e0 || n > (long long)1e8) {
+  benchmark = atoll(av[2]);
+  tax_rate = atof(av[3]);
+  if (n < (long long)1e1 || n > (long long)1e8) {
     cerr << "invaild argument:" << '\n';
     cerr << "  n:" << '\n';
-    cerr << "    1e0 <= n && n <= 1e8" << '\n';
+    cerr << "    1e1 <= n && n <= 1e8" << '\n';
     return -2;
-  }
-  if (mn < (long long)1e0 || mn > (long long)1e12) {
-    cerr << "invaild argument:" << '\n';
-    cerr << "  min:" << '\n';
-    cerr << "    1e0 <= min && min <= 1e12" << '\n';
-    return -3;
-  }
-  if (mx < (long long)1e0 || mx > (long long)1e12) {
-    cerr << "invaild argument:" << '\n';
-    cerr << "  max:" << '\n';
-    cerr << "    1e0 <= max && max <= 1e12" << '\n';
-    return -4;
-  }
-  if (mn > mx) {
-    cerr << "invaild argument:" << '\n';
-    cerr << "  min & max:" << '\n';
-    cerr << "    min <= max" << '\n';
-    return -5;
   }
   if (benchmark < (long long)1e0 || benchmark > (long long)1e12) {
     cerr << "invaild argument:" << '\n';
     cerr << "  benchmark:" << '\n';
     cerr << "    1e0 <= benchmark && benchmark <= 1e12" << '\n';
-    return -6;
+    return -3;
   }
   if (tax_rate < 1e-5 || tax_rate > 1e5) {
     cerr << "invaild argument:" << '\n';
     cerr << "  tax_rate:" << '\n';
     cerr << "    1e-5 <= tax_rate && tax_rate <= 1e5" << '\n';
-    return -7;
+    return -4;
   }
   return 0;
 }
@@ -71,13 +47,18 @@ template<class T> T __random(T mnm, T mxm) {
   static mt19937 gen((random_device())());
   return (uniform_int_distribution<T>(mnm, mxm))(gen);
 }
-void random_generate(vector<long long> &property,
-                     long long n,
-                     long long mn,
-                     long long mx) {
+void generate_random(vector<long long> &property, long long n) {
+  size_t const sect = 5;
+  long long div = n / sect;
+  long long rmn = n % sect;
+  long long val = 100'000;
+  size_t ix = 0;
   property.resize(n);
-  for (size_t i = 0; i < n; ++i) {
-    property[i] = __random(mn, mx);
+  for (size_t i = 0; i < sect; ++i) {
+    for (size_t j = 0; j < div + (rmn > i); ++j) {
+      property[ix++] = __random(val, val * 10);
+    }
+    val *= 10;
   }
 }
 void tax_rate_calculate_before(vector<long long> const &property,
@@ -111,8 +92,9 @@ void tax_rate_calculate_after(vector<long long> const &property,
   size_t n = property.size();
   after.resize(n);
   for (size_t i = 0; i < n; ++i) {
-    after[i] = (tax_rate / 1e2) *
-        ((double)property[i] / (double)benchmark) * property[i];
+    double tax_rate_ = tax_rate / 1e2;
+    double ratio = (double)property[i] / (double)benchmark;
+    after[i] = min(tax_rate_, tax_rate_ * ratio) * property[i];
   }
 }
 template<class T> string const &regex_money(T value, size_t unit) {
@@ -129,33 +111,29 @@ template<class T> string const &regex_money(T value, size_t unit) {
   s2.pop_back();
   return s2;
 }
-void print_vector(vector<long long> const &v, size_t unit) {
-  for (size_t i = 0; i < v.size(); ++i) {
-    cout << regex_money(v[i], unit) << " \n"[i == v.size() - 1];
-  }
-}
 int main(int ac, char **av) {
   ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
   vector<long long> property, before, after;
-  long long n, mn, mx, benchmark;
+  long long n, benchmark;
   double tax_rate;
   size_t unit = 3;
-  if (int tmp = argument_check(ac, av, n, mn, mx, benchmark, tax_rate)) {
+  if (int tmp = argument_check(ac, av, n, benchmark, tax_rate)) {
     return tmp;
   }
-  random_generate(property, n, mn, mx);
+  generate_random(property, n);
   tax_rate_calculate_before(property, before);
   tax_rate_calculate_after(property, after, benchmark, tax_rate);
-  if (n <= 20) {
-    print_vector(property, unit);
-    print_vector(before, unit);
-    print_vector(after, unit);
+  if (n <= 100) {
+    for (size_t i = 0; i < n; ++i) {
+      cout << i + 1 << ":\t" <<
+          regex_money(property[i], unit) << "    " <<
+          regex_money(before[i], unit) << "    " <<
+          regex_money(after[i], unit) << '\n';
+    }
   }
-  cout << regex_money(
-      accumulate(property.begin(), property.end(), 0ull), unit) << '\n';
-  cout << regex_money(
-      accumulate(before.begin(), before.end(), 0ull), unit) << '\n';
-  cout << regex_money(
-      accumulate(after.begin(), after.end(), 0ull), unit) << '\n';
+  cout << "total:\t" << 
+      regex_money(accumulate(property.begin(), property.end(), 0ull), unit) << "    " <<
+      regex_money(accumulate(before.begin(), before.end(), 0ull), unit) << "    " <<
+      regex_money(accumulate(after.begin(), after.end(), 0ull), unit) << '\n';
   return 0;
 }
